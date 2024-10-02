12220da60327dabbbe079742fa4d90cd88591a6e
jaydenhuang555 <jaydenkanahuang@outlook.com>
2024-10-02 08:29:38 -0700

    fixed rando stuff with stack logic for calc, still no Makefile LOL

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,26 +1,69 @@
 #include "Calculator.h"
 #include "Token.h"
 #include "stdlib.h"
+#include "stdio.h"
 namespace jay {
 template<class T>
 struct Stack {
 	struct Node {
 		T item;
-		Node(T item) : item(item){}
+		struct Node *next;
+		struct Node *prev;
+		Node() : next(0) , prev(0) {}
+		Node(T item) : item(item), next(0), prev(0) {}
 	};
 	unsigned long cap;
-	T *head;
-	T *tail;
+	struct Node *head;
+	Node *tail;
 	Stack(){
-		this->cap = 32;
-		head = (T*)malloc(sizeof(T) * cap);
+		head = new Node();
+		tail = head;
+	}
+	void push(T item){
+		tail->next = new Node(item);
+		tail->next->prev = tail;
+		tail = tail->next;
+	}
+	T peek(){
+		return tail->item;
+	}
+	T pop(){
+		T item = peek();
+		Node *node = tail->prev;
+		
+		delete tail;
+		tail = node;
+		return item;
+	}
+	unsigned int isEmpty(){
+		return head == tail ? true : false;
+	}
+
+	~Stack(){
+		for(Node *next = head; next;){
+			Node *node = next;
+			next = next->next;
+			delete node;
+		}
 	}
 };
-Token *tokens = (Token*)0;
+Token *tokens = 0;
+
+void test(){
+	Stack<const byte*> *stack = new Stack<const byte*>();
+	stack->push("test");
+	stack->push("lkjhg");
+	while(!stack->isEmpty()) printf("%s\n", stack->pop());
+	delete stack;
+}
 
 Calculator::Calculator(){
+	test();
 }
-}
+
+} // jay
+
+
 
 
 
--- main.cpp
+++ main.cpp
@@ -2,16 +2,15 @@
 #include "dlfcn.h"
 #include "defs.h"
 #include "signal.h"
+#include "Calculator.h"
 
 int main(int argc, byte **argv){
 	const byte *test1 = "1+2*3";
 	const byte *test2 = "10+20*30";
 	const byte *test3 = "-10-20(38)";
-
-	void *handler = dlopen("calc.so");
-	if(!handler){
-		printf("error when opeing so: %s" ,dlerror());
-		raise(SIGABRT);
-	}
+	
+	jay::Calculator *calc = new jay::Calculator();
+	delete calc;
+	
 
 }
